800b13037850724d5020415e80e555464d1727e1
HaiBin <haibinmath@gmail.com>
2017-12-23 15:03:50 +0800

    Basic script

---
+++ PipeForward/PipeForward/Script01.fsx
@@ -0,0 +1,92 @@
+﻿// You can use double-slash //, 
+// triple-slash ///, 
+// or star-brackets (* ....... *) to make comments in F#.
+
+///////////////////////////////////////////
+// In Visual-Studio in Windows, you can highlight a certain section of code, and
+// run it in FSharp-interactive using "ALT + ENTER".
+// Alternatively, highlight the code you want to run, rightclick, and 
+// "Execute in interactive"
+
+// Remark: I am not sure how to do it in Mac computers.
+
+// Try it out with the code below.
+
+let x = 1
+let y = x + 5
+printfn "The result is: %i" y
+
+// printfn is to print the result to the console. 
+// %i is for integer, %f for decimal/double, %s for string, %A for general objects.
+// No need to remember the "%i", "%s", etc.
+
+///////////////////////////////////////////
+// As seen above, you can define a variable using "let"
+
+// Also, a variable in F# is by default "immutable", which means it cannot change value.
+// So, if you try to reassign a value to "x", an error will occur.
+// You can un-comment the code below and see what happens. 
+
+// Warning: Will create error if uncommented.
+// x <- 2
+
+
+
+// You can use "mutable" or changable variables in F#,
+// but we do NOT recommend using "mutable" variables.
+// And we will NOT show it here.
+
+/////////////////////////////////////////////////////////////////
+
+// Also, F# uses "=" and "<>" for equality/ inequality checking. 
+// (Unlike "==" in Java/C++, and "===" in JavaScript)
+let valueToTest = 20
+let isValueEqualToTwenty = (valueToTest = 20)
+
+if isValueEqualToTwenty then
+    printfn "Yes, the value is Twenty"
+else 
+    printfn "No, the value is not Twenty"
+
+// The second line "No, ......" will not be printed.
+
+///////////////////////////////////////////////////////////////////
+// You can define integer, double, string, 
+// and these types support the "+" operation.
+
+let number1 = 40
+let number2 = 55
+let addTwoNumbers = number1 + number2
+
+let sqrtTwoApprox = 1.414
+let piApprox = 3.1415926
+let addTwoDecimals = sqrtTwoApprox + piApprox
+
+let sentenceStart = "My school is "
+let schoolName = "National University of Singapore"
+let combinedSentence = sentenceStart + schoolName
+
+///////////////////////////////////
+// However, you cannot combine two different types using the "+" functions
+// The code all below, if un-commented, will create error.
+
+// let addIntegerWithDecimal = 15 + 4.11
+// let combineStringWithInteger = "My age is: " + 21
+
+// This is an implicit casting that you may take for granted in other languages (e.g. Java, C++, Javascript)
+// But in F#, this strict rule will help us make less mistake when we write our code.
+
+////////////////////////////////////
+// Similarly, some functions (such as squareRoot "sqrt") only accepts double/float/decimals, 
+// So be careful if you want to calculate it for Integers.
+
+let sqrtRootOfNine = sqrt 9.0
+let twoToPowerOfFive = 2.0 ** 5.0 
+// 2^5 = 2*2*2*2*2 = 32
+
+// ERROR: sqrt and (power ** ) only accepts double/decimals/float
+//let sqrtRootOfNineError = sqrt 9
+//let twoToPowerOfFiveError = 2 ** 5 
+
+/////////////////////////////////////////////////////////////////
+// Defining functions.
