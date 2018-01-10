857011545481c49240bb80ba28aede1627655299
HaiBin <haibinmath@gmail.com>
2018-01-10 23:47:13 +0800

    Script05.

--- PipeForward/PipeForward/Script05.fsx
+++ PipeForward/PipeForward/Script05.fsx
@@ -1,17 +1,77 @@
 ﻿// Key-concept: 
-// 1. Code in F# are very easy to understand.
-// 1a. The pipe-forward operator and the F# language design helps.
+// 1. First look at List function
+// 1a. List.filter
+// 1b. List.map
 
-// 2. Anonymous function/lambda function also helps.
-// 2a. You are defining a function at the exact location where it is most useful.
-// 2b. And so it boosts productivity.
-// 2c. "fun" is a keyword in F#! 
+// 2. Code in F# are very easy to understand.
+// 2a. The pipe-forward operator and the F# language design helps.
+
+// 3. Anonymous function/lambda function also helps.
+// 3a. You are defining a function at the exact location where it is most useful.
+// 3b. And so it boosts productivity.
+// 3c. "fun" is a keyword in F#! 
 
 
+
+// We can create a list of integers/double/string using the following notation:
+let list1 = [1 .. 100]          
+let list2 = [50 .. 80]
+let list3 = [1 .. 2 .. 100]      
+
+let list4 = [1.0 .. 100.0]      
+let list5 = [0.0 .. 0.05 .. 1.0]
+
+let list6 = [1; 20; 50; 100; 55; 5; 10]
+let list7 = [1.0; 6.0; 5.0; 10.0; 3.0; 2.0]
+
+let list8 = ["ABC";"DEF";"GHI";"JKL";"MNO"]
+
+// ERROR: Cannot have a list with different types.
+//let listError = ["ABC"; 123; 400.0]
+
+
+////////////////////////////////////////////////////////////////////////////////////
+
+// Here is a simple function that returns true/false.
+let IsItEven x = (x % 2 = 0)
+
+// x % 2 mean the remainder after we divide x by 2
+// (x % 2 = 0) means "is the remainder (after the division by 2) equal to zero?"
+// i.e. is x divisible by 2?
+
+let trueOrFalse1 = IsItEven 10
+let trueOrFalse2 = IsItEven 3
+
+
+/////////////////////////////////////////
+// List.filter
+
+let result1 = List.filter IsItEven [1 .. 100]
+
+// The List.filter function filters a list, and only select the elements which satisfy some requirement.
+// The requirement is specified through a function "IsItEven".
+
+
+// Alternatively, because the definition of IsItEven is quite easy, we can even implement it immediately after
+// List.filter, at the point where we need it the most.
+
+let result2 = List.filter (fun x -> x % 2 = 0) [1 .. 100]
+
+// Here, we replaced "IsItEven" in the code above with:
+// 1. "fun":   A function......            "fun" is a keyword in F# !
+// 2.   x  : ...that takes an input x...
+// 3.  ->  : ...and returns...
+// 4.  x%2=0: ... whether x is divisible by 2
+
+// Again, we don't want to waste time figuring out a name for our function "IsItEven", 
+// If we define too many such custom function, it will be hard to keep track,
+// And we will lose productivity.
+
+// We define this function using the "fun" keyword at the exact location where we need it.
 
 
 ////////////////////////////////////
-// Example:
+// Let's say we apply this function with the pipe-forward operator "|>"
 
 // Take a look at this function:
 
@@ -26,21 +86,21 @@ let SumOfMultiplesOfThree (xList: List<int>) =
 // 2. and apply this input (using the first |> symbol) to the first function.
 
 // 2 a. This first function filters (List.filter) the input list
-// 2 b. So that only multiples of 3 are accepted/retained (x % 3 = 0)
-// 2 c. The filtering process requires a function (fun x -> ......)
+// 2 b. So that only multiples of 3 are accepted/retained (fun x -> x % 3 = 0)
 
 // 3. After the previous process is done, we have an intermediateResult.
 // 3 a. Then we apply this intermediateResult (using the second |> symbol) to the second function.
 // 3 b. This second function just sums up (List.sum) the remaining elements after the filtering process.
 
 // So, F# is able to express all of these calculations with just 3 lines of code.
-// Which is quite elegant, compared to other more traditional languages (Java/C++)
+// Which is quite elegant, maybe similar to Python code (in style)
+// compared to other more traditional languages (Java/C++) which we need to write longer.
 
-let result1 = SumOfMultiplesOfThree [1 .. 1000]
-printfn "The sum of multiples of 3, starting from 1 to 1000 is: %i" result1
+let result3 = SumOfMultiplesOfThree [1 .. 1000]
+printfn "The sum of multiples of 3, starting from 1 to 1000 is: %i" result3
 
-let result2 = SumOfMultiplesOfThree [1 .. 20000]
-printfn "The sum of multiples of 3, starting from 1 to 20000 is: %i" result2
+let result4 = SumOfMultiplesOfThree [1 .. 20000]
+printfn "The sum of multiples of 3, starting from 1 to 20000 is: %i" result4
 
 
 
@@ -61,21 +121,10 @@ let SumOfMultiplesOfThree_Version2 (xList: List<int>) =
     |> List.filter IsDivisibleByThree
     |> List.sum
 
-// Here, we painfully define a new function "IsDivisibleByThree" to determine whether the input "x"
-// is divisible by 3 or not. 
-
-// And so, the "List.filter" function happily accepts the "IsDivisibleByThree" function,
-// even though the previous version "fun x -> x % 3 = 0" is also acceptable.
-
-// But using the previous notation "fun x -> x % 3 = 0" is better, because:
-// 1. You don't need to waste time to think of a name for your function. 
-// 2. Once you get used to the syntax/writing style, you see that "IsDivisibleByThree" is 
-//    no more helpful than "(fun x -> x % 3 = 0)"
-// 2a. In fact, if lets say you used "IsDivisibleByThree" after 10,000 lines of code,
-//     then in order to know what is going on in the "IsDivisibleByThree" function,
-//     you need to go through tons of code in order to find the implementation.
-
-
+// Here, we have to waste time, to painfully define a new function "IsDivisibleByThree" 
+// to determine whether the input "x" is divisible by 3 or not. 
+// Which means it is something extra that we need to keep track of, which is bad.
+// Just like Python, we hope to aim for minimally understandable code that is easy to maintain.
 
 
 //////////////////////////////////////////////////////////////////////////////////////
@@ -102,9 +151,8 @@ let CountGreaterThan80 (scoreList: List<int>) =
 // 3 b. This second function just counts how many remaining students are there.
 // 3 c. By finding the length (List.length) of the remaining List
 
-let result3 = CountGreaterThan80 [60; 65; 70; 75; 80; 85; 90; 95]
-printfn "This class has %i students scoring more than 80." result1
-
+let result5 = CountGreaterThan80 [60; 65; 70; 75; 80; 85; 90; 95]
+printfn "This class has %i students scoring more than 80." result5
 
 ////////////////////////////////////////
 // Again, we could have defined a new function, "IsAtLeast80" to replace the anonymous function.
@@ -125,33 +173,122 @@ let CountGreaterThan80_Version2 (scoreList: List<int>) =
 // But it makes it harder to understand, which is not helpful.
 
 /////////////////////////////////////////////////////////////////////////////////////
+// Example:
+let SumMultiplesOf3ButNot5 (xList: List<int>) =
+    xList
+    |> List.filter (fun x -> x % 3 = 0 && x % 5 <> 0)
+    |> List.sum
+
+// The function used to filter is saying the following:
+// (x % 3 = 0): is x divisible by 3?
+// (x % 5 <> 0): is x NOT a multiple of 5?
+
+// So we include multiples of 3, e.g. 3, 6, 9, 12,......
+// but we ignore multiples of 5, e.g. 15, 30, 45,.......
+let result6 = SumMultiplesOf3ButNot5 [1 .. 100]
+printfn "The sum is: %i" result6
+
+/////////////////////////////////////////////////////////////////////////////////////
 // Exercise: 
 // Implement a function that sums up all multiples of 3 or 5 in a list.
 let SumMultiplesOf3Or5 (xList: List<int>) =
-
+    // Hint:  || means OR, && means AND
     failwith "NOT IMPLEMENTED!" 
 
 // Within 1 to 10, the multiples of 3 or 5 are 3, 5, 6, 9, 10
 // 3 + 5 + 6 + 9 + 10 = 33
-let result4 = SumMultiplesOf3Or5 [1 .. 10]
-printfn "The result is: %i" result4
+let result7 = SumMultiplesOf3Or5 [1 .. 10]
+printfn "The result is: %i" result7
 
 // You can create an account, and submit your answers here for personal achievement/accomplishment.
 // https://projecteuler.net/problem=1
-let result5 = SumMultiplesOf3Or5 [1 .. 999]
-printfn "The sum of all multiples of 3 or 5, from 1 to 999 is: %i" result5
+let result8 = SumMultiplesOf3Or5 [1 .. 999]
+printfn "The sum of all multiples of 3 or 5, from 1 to 999 is: %i" result8
   
 
   
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
+// List.map
+
+let Square x = x * x
+let result9 = List.map Square [1 .. 10]
+
+
+// The List.map function transform the individual element of a list using some transformation
+// The transformation is specified through a function "Square".
+
+
+// Alternatively, we can use the "fun" keyword to define the "Square" function
+
+let result10 = List.map (fun x -> x * x) [1 .. 100]
+
+// Here, we replaced "IsItEven" in the code above with:
+// 1. "fun":   A function......            "fun" is a keyword in F# !
+// 2.   x  : ...that takes an input x...
+// 3.  ->  : ...and returns...
+// 4.  x*x: ... x multiplied with x
+
+// Again, we define this function using the "fun" keyword at the exact location where we need it.
+// It increases productivity.
+
+////////////////////////////////////////
+// Let's see it together with the pipe-forward operator:
+
+// Another example:
+let SumOfSquares (xList: List<int>) =
+    xList
+    |> List.map (fun x -> x * x)
+    |> List.sum
+
+// What this code does is the following:
+
+// 1. It starts with a List of integers (xList)
+
+// 2. and apply this input (using the first |> symbol) to the first function.
+
+// 2 a. This first function transforms/maps each element (List.map) in the input list
+// 2 b. Each number is converted/mapped to the square of itself (fun x -> x * x)
+// 2 c. The filtering process requires a function (fun x -> ......)
+
+// 3. After the previous process is done, we have an intermediateResult, a List of remaining scores.
+// 3 a. Then we apply this intermediateResult (using the second |> symbol) to the second function.
+// 3 b. This second function sums up all the elements inside. (List.sum)
+
+// 1^2 + 2^2 + 3^2 + 4^2 + ... + 10^2 = 385
+let result11 = SumOfSquares [1 .. 10]
+printfn "Sum of squares from 1 to 10 is: %i" result11
+
+
+
 
 // Continue to write tutorial here.......................
 
 // To be completed by HaiBin................
 
-
+/////////////////////////////////////////////////////////////////
+// Fill in the code below, that will help you solve Problem6 in Project Euler (see the URL)
 
 // You can create an account, and submit your answers here for personal achievement/accomplishment.
 // https://projecteuler.net/problem=6
+
+let ProjectEulerProblem6 (xList: List<int>) =
+    // if xList = [a;b;c], calculate a^2 + b^2 + c^2
+    let sumOfSquares = 
+        failwith "NOT YET IMPLEMENTED!"
+
+    // if xList = [a;b;c], calculate a + b + c
+    let sum =
+        failwith "NOT YET IMPLEMENTED!"
+
+    // return
+    (sum * sum) - sumOfSquares
+
+
+
+
+///////// Test:
+let result12 = ProjectEulerProblem6 [1 .. 100]
+printfn "The result for ProjectEuler Problem6 is: %i" result12
+
--- PipeForward/PipeForward/Script06.fsx
+++ PipeForward/PipeForward/Script06.fsx
@@ -22,6 +22,8 @@
 // But you can consider the machine as a real concrete "thing", just like how an apple is a real concrete "thing".
 
 
+
+
 // How this analogy compares with programming:
 
 // We have seen before that we can define a function like this:
@@ -57,4 +59,39 @@ printfn "The result is: %i" (f2 200)
 // 3. The output is also an integer, slightly larger than the input.
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Let's look at the code before:
+let result1 = List.filter (fun x -> x % 2 = 0) [1 .. 100]
+
+// Hover your mouse on top of "List.filter", and you will see the following signature:
+
+//    input1    ->  input2 ->  output
+// ('T -> bool) -> 'T List -> 'T List
+
+// What this means is that List.filter accepts two input:
+// input1: A function that accepts one element (in our case, an integer)
+//         And tells us whether it satisfy some requirement (through a boolean true/false output)
+// input2: A list of item (in out case, List of integers) that we want to filter on
+
+// output: A potentially shorter list, removing all elements that return "false" on the function "input1"
+
+// Notice that input1 is a function! It is a "thing"!
+
+
+
+// So, this means that "List.filter" is actually a bigger function/machine!
+// You can think of "List.filter" as a bigger machine/factory.
+
+// It needs a detector/scanner/function to determine whether something is good or bad (true/false).
+// Then it needs another input, which is a list of objects to apply this scanner/detector on.
+
+// Once the detector/scanner and the List of objects are ready in the big machine/factory,
+// It runs the checking process, and throw away things that are bad (false)
+
+// And finally it returns the processed and remaining List.
+
+
+
+// In this case, a bigger machine/factory can accept a function/smaller scanner machine as input!
+// The scanner/detector is actually a thing! You can give this "thing" to another bigger entity/machine/factory.
 
