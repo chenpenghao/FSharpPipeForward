35ccc1b2e3964fa943f47453af764c90d22d62dc
HaiBin Chang <haibinmath@gmail.com>
2018-02-10 17:33:54 +0800

    Script02

--- PipeForward/PipeForward/Script02.fsx
+++ PipeForward/PipeForward/Script02.fsx
@@ -1,4 +1,4 @@
-﻿// Section1: Introduction
+﻿// 2.1: Introduction
 
 // You do not need to implement the pipe-forward operator.
 // It is already defined in F#.
@@ -6,7 +6,7 @@
 // let inline (|>) x f = f x
 
 /////////////////////////////////////////////////////////////////////////
-// Section2: Simple demonstration
+// 2.2: Simple demonstration
 
 let Add5Func x = x + 5
 
@@ -18,7 +18,7 @@ let result2 = 30 |> Add5Func
 printfn "Result is: %i" result2
 
 ///////////////////////////////////////////////////////////
-// Section3: Usefulness
+// 2.3: Usefulness
 
 // Let's say that you are given these two functions:
 let GetGrade score =
@@ -47,7 +47,7 @@ let GetGPA grade =
 ////////////////////////////////
 
 // verbose/long version
-let GetCAPfromScore1 (score:int) =
+let GetCAPfromScore1 score =
     let intermediateResult = GetGrade score
     let finalResult = GetCAP intermediateResult
     // return
@@ -65,19 +65,12 @@ printfn "Your CAP is: %f" cap2
 // GetCAP:           string ->  float
 
 // Shorter version
-let GetCAPfromScore2 (score:int) =
+let GetCAPfromScore2 score =
     score               
     |> GetGrade         
     |> GetCAP           
 
-// This is interpreted as:
-//
-// First, you start off with a "score" integer.
-// Then, you apply this variable (the first "|>" symbol) to the function "GetGrade"
-// Finally, whatever intermediate result you get after the "GetGrade" function, 
-// You apply the intermediate result (the second "|>" symbol) to the function "GetCAP"
-// And since "GetCAP" is the final line of the "CombinedFunction2",
-// Whatever result that comes after "GetCAP" will be the output of the CombinedFunction2.
+// See the pdf for a step-by-step explanation of the code above.
 
 let cap3 = GetCAPfromScore2 95
 printfn "Your CAP is: %f" cap3
@@ -85,41 +78,27 @@ printfn "Your CAP is: %f" cap3
 let cap4 = GetCAPfromScore2 85
 printfn "Your CAP is: %f" cap4
 
-// Notice that the order of the function operation matters, 
-// And so if you apply the function in the wrong order, it may not compile.
+///////////////////////////////
+// function will not compile if it is in the wrong order.
 
 // uncomment to see errors.
 // ERROR: Functions applied in the wrong order.
 //let CombinedFunction3Error (score:int) =
 //    score       // int
-//    |> GetCAP   // function: string -> double   // ERROR: GetCAP accepts string, not integer!
-//    |> GetGrade // function: int -> string      // ERROR: GetGrade accepts int, not double (the result of GetCAP).
-
-
-
-
+//    |> GetCAP   // function: string -> double   // ERROR
+//    |> GetGrade // function: int -> string      // ERROR
 
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-// Let's try another (hypothetical) example:
-
-// Let's imagine a scenario where you want to buy or sell a company's stock
-// Based on the company's performance relative to the financial analyst's estimate.
+// 2.4 More examples
 
 // First, F# has a built-in function, "List.average"
-// that find the average of a list of numbers:
 let average1 = List.average [1.0; 2.0; 3.0; 4.0; 5.0]
 let average2 = List.average [80.0; 85.0; 90.0; 95.0; 100.0]
 
+/////////////////////////////
 
-
-// In addition, let's say that the following 2 functions are available to you:
-
-// Assume that the actual profit of the company is $ 6.0 billion for that year.
-// If the actual profit exceed the analystEstimate by 5%, then we say it is "OUTPERFORM"
-// If the actual profit misses the analystEstimate by 5%, then we say it is "UNDERPERFORM"
-// else, "NEUTRAL"
-let GetPerformance (analystAverageEstimate: double) =
+let GetPerformance analystAverageEstimate =
     let actualProfit = 6.0
     if actualProfit > analystAverageEstimate * 1.05
         then "OUTPERFORM"
@@ -128,46 +107,41 @@ let GetPerformance (analystAverageEstimate: double) =
     else 
         "NEUTRAL"
 
-let GetNumSharesToBuy (performance: string) =
+let GetNumSharesToBuy performance =
     if performance = "OUTPERFORM" then
-        100     // buy 100 shares
+        1000     // buy 1000 shares
     else if performance = "UNDERPERFORM" then
-        -100    // sell 100 shares
+        -1000    // sell 1000 shares
     else 
         0       // hold.
-
-// So we have three functions available to us:
+        
 // List.average:        List<double> -> double
-// GetPerformance:      double -> string
-// GetNumSharesToBuy:   string -> int
+// GetPerformance:                      double -> string
+// GetNumSharesToBuy:                             string -> int
 
-// In this carefully crafted example, notice that the result of the one function 
-// can act as the input to the other function.
-// So, we can combine them into a big function:
+//////////////////////////////////////////////////////////////////////////////
+// Long version
 
 // Assume the profit is already known to be $6.0 billion, and written in "GetPerformance"
-let GetNumSharesFromEstimate1 (individualEstimates: List<double>) =
+let GetNumSharesFromEstimate1 individualEstimates =
     let intermediateResult1 = List.average individualEstimates
     let intermediateResult2 = GetPerformance intermediateResult1
     let finalResult = GetNumSharesToBuy intermediateResult2
     // output
     finalResult
-
-// Notice that the code above uses a lot of temporary variables "intermediateResult1", etc.
-// which makes the code longer. Nonetheless, we can still use it.
-
+    
 // In this example, the actual profit (6.0 billion) exceeds all the financial analyst's prediction.
 // Which means this is good news.
 let numShares1 = GetNumSharesFromEstimate1 [4.0; 5.0; 3.0; 2.0; 2.5]
-printfn "Number of shares to buy(positive) or sell(negative): %i" numShares1
+printfn "Number of shares to buy(+) or sell(-): %i" numShares1
 
 // In this example, the actual profit (6.0 billion) misses all the financial analyst's prediction.
 // Which means this is bad news.
 let numShares2 = GetNumSharesFromEstimate1 [8.0; 7.0; 10.0; 12.0; 10.5]
-printfn "Number of shares to buy(positive) or sell(negative): %i" numShares2
+printfn "Number of shares to buy(+) or sell(-): %i" numShares2
 
 /////////////////////////
-
+// Original:
 //let GetNumSharesFromEstimate1 (individualEstimates: List<double>) =
 //    let intermediateResult1 = List.average individualEstimates
 //    let intermediateResult2 = GetPerformance intermediateResult1
@@ -175,27 +149,13 @@ printfn "Number of shares to buy(positive) or sell(negative): %i" numShares2
 //    // output
 //    finalResult
 
-// The original version looks like the above, which we can simplify:
-
-let GetNumSharesFromEstimate2 (individualEstimates: List<double>) =
+// Simplified:
+let GetNumSharesFromEstimate2 individualEstimates =
     individualEstimates                 
     |> List.average                     
     |> GetPerformance                  
     |> GetNumSharesToBuy                
-
-// So, we start off with "individualEstimates" a List of doubles/decimals.
-//     Now, we apply (using the  first |> symbol) this starting variable
-//              to the first function "List.average" 
-
-// and then we apply (using the second |> symbol) this intermediateResult
-//              to the  next function "GetPerformance"
-
-// and then we apply (using the  third |> symbol) the intermediateResult before
-//              to the  last function "GetNumSharesToBuy"
-
-// and since "GetNumSharesToBuy" is the last line, 
-// whatever results after that is our output of the combined function.
-
+    
 
 
 // And lets try our new function here.
@@ -210,46 +170,31 @@ printfn "Number of shares to buy(positive) or sell(negative): %i" numShares3
 let numShares4 = GetNumSharesFromEstimate2 [8.0; 7.0; 10.0; 12.0; 10.5]
 printfn "Number of shares to buy(positive) or sell(negative): %i" numShares4
 
-// The benefit of writing like such is that we don't need additional clutter/ unnecessary words on 
-// our computer screen (to represent the intermediate result), and we can focus more on the 
-// internal logic/calculations and not worry about the tiny details.
-
-// And so, you will be able to make less mistake (and be more productive writing code)
-// And the code will be easy to read for other programmers used to the syntax.
-
-// Once you get used to this syntax, you may find other traditional programming language 
-// e.g. Java/ C++ to be a bit verbose/too long.
-
 ///////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
+// 2.6 Intellisense
 // If you are not comfortable with completely getting rid of intermediateSteps, you can try the following:
 
-let PartiallyCompletedFunction1 (individualEstimates: List<double>) =
+let myFunction1 (individualEstimates: List<float>) =
+    individualEstimates                 
+    |> List.average          
+
+let myFunction2 individualEstimates =
     individualEstimates                 
     |> List.average                     
-    // |> GetPerformance          
-    // |> GetNumSharesToBuy
+    |> GetPerformance        
 
-let PartiallyCompletedFunction2 (individualEstimates: List<double>) =
-    individualEstimates                 
-    |> List.average                     
-    |> GetPerformance                    
-    // |> GetNumSharesToBuy
-
-let CompletedFunction (individualEstimates: List<double>) =
+let myFunction3 individualEstimates =
     individualEstimates                 
     |> List.average                     
     |> GetPerformance                  
     |> GetNumSharesToBuy
 
-// You can put your mouse on top of the "PartiallyCompletedFunction1", "PartiallyCompletedFunction2", etc.
+// Hover your mouse on top of the "myFunction1", "myFunction2", "myFunction3" 
 // and see that the functions are
-// PartiallyCompletedFunction1: List<double> -> double
-// PartiallyCompletedFunction2: List<double>            -> string
-// CompletedFunction:           List<double>                       -> int
-
-// In practice, you will slowly write line by line, 
-// and keep track of what is the output until that point (by hovering your mouse on top of the functions)
+// myFunction1: List<double> -> double
+// myFunction2: List<double>            -> string
+// myFunction3: List<double>                       -> int
 
 ////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////
@@ -264,20 +209,15 @@ let CompletedFunction (individualEstimates: List<double>) =
 let sum1 = List.sum [1.0; 2.0; 3.0; 4.0; 5.0]   // sum from 1 to 5
 let sum2 = List.sum [1.0 .. 100.0]              // sum from 1 to 100
 
-// In addition, you are given the following 2 functions:
-
-// If the employee brings in $10.0 million this year, he is one of the best trader in the company. 
-// If the employee loses $3.0 million this year, he is causing huge losses to the company.
-// Otherwise, he is a regular trader.
-let GetStatus (profit: double) =
+let GetStatus profit =
     if profit > 10.0 then
         "TOP TRADER"
     else if profit < -3.0 then
-        "CAUSE HUGE LOSSES TO COMPANY"
+        "HUGE LOSSES"
     else
         "NORMAL TRADER"
 
-let GetBonus (status: string) =
+let GetBonus status =
     if status = "TOP TRADER" then   
         24      // 24-month, i.e. 2 year bonus.
     else if status = "NORMAL TRADER" then
@@ -287,19 +227,22 @@ let GetBonus (status: string) =
 
 // The functions are:
 // List.sum : List<double> -> double
-// GetStatus: double -> string
-// GetBonus:  string -> int
+// GetStatus:                 double -> string
+// GetBonus:                            string -> int
 
-// An implementation combining the function would look like:
-let GetBonusFromTrades1 (listOfTrades: List<double>) =
+/////////////////////////////////////
+// Long version
+let GetBonusFromTrades1 listOfTrades =
     let intermediateResult1 = List.sum listOfTrades
     let intermediateResult2 = GetStatus intermediateResult1
     let finalResult = GetBonus intermediateResult2
     // output
     finalResult
 
+// Exercise:
+
 // Try implementing it with the pipe forward operator "|>"
-let GetBonusFromTrades2 (listOfTrades: List<double>) =
+let GetBonusFromTrades2 listOfTrades =
     // delete the line below, and fill in your answer 
 
     failwith "FUNCTION NOT YET IMPLEMENTED!"
